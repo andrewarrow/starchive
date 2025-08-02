3095201db8e60ef7f8cf8483b09690a624285c31
aa <oneone@gmail.com>
2025-08-02 08:45:34 -0700

    video id from url

--- firefox/background.js
+++ firefox/background.js
@@ -9,4 +9,24 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
       });
     return true;
   }
+  
+  if (msg.type === "youtubeVideo") {
+    fetch("http://localhost:3009/youtube", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ videoId: msg.videoId })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log("YouTube video ID sent:", msg.videoId, "Response:", data);
+        if (sendResponse) sendResponse(data);
+      })
+      .catch(err => {
+        console.error("Error sending YouTube video ID:", err);
+        if (sendResponse) sendResponse({ error: err.message });
+      });
+    return true;
+  }
 });
--- firefox/content.js
+++ firefox/content.js
@@ -1,1 +1,27 @@
+function checkForYouTubeVideo() {
+  if (window.location.hostname === 'www.youtube.com' || window.location.hostname === 'youtube.com') {
+    const urlParams = new URLSearchParams(window.location.search);
+    const videoId = urlParams.get('v');
+    
+    if (videoId && window.location.pathname === '/watch') {
+      chrome.runtime.sendMessage({ 
+        type: "youtubeVideo", 
+        videoId: videoId 
+      });
+    }
+  }
+}
+
+checkForYouTubeVideo();
+
+let lastUrl = window.location.href;
+const observer = new MutationObserver(() => {
+  if (lastUrl !== window.location.href) {
+    lastUrl = window.location.href;
+    checkForYouTubeVideo();
+  }
+});
+
+observer.observe(document, { subtree: true, childList: true });
+
 chrome.runtime.sendMessage({ type: "fetchData" });
