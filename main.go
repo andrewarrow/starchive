2e953e5af4a256e17eb7d04c7b3281ab248f5a19
aa <oneone@gmail.com>
2025-08-02 08:46:27 -0700

    adding post method

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -9,6 +11,29 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Request received:", r.Method, r.URL.Path)
 		fmt.Fprintf(w, "hello world")
+	})
+	
+	http.HandleFunc("/youtube", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+		
+		var jsonData interface{}
+		if err := json.Unmarshal(body, &jsonData); err != nil {
+			fmt.Printf("Invalid JSON received: %s\n", string(body))
+		} else {
+			fmt.Printf("JSON received: %+v\n", jsonData)
+		}
+		
+		fmt.Fprintf(w, "Received")
 	})
 	
 	fmt.Println("Server starting on port 3009...")
