7ef138d86cd631b2d79ac58915ff6219f712012f
aa <oneone@gmail.com>
2025-08-11 17:04:32 -0700

    adding sync

--- commands.go
+++ commands.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -180,6 +181,120 @@ func handleBpmCommand() {
 		fmt.Printf("Warning: Could not store BPM data in database: %v\n", err)
 	} else {
 		fmt.Printf("\nBPM data stored in database for %s\n", id)
+	}
+}
+
+func handleSyncCommand() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: starchive sync <id1> <id2>")
+		fmt.Println("Example: starchive sync NdYWuo9OFAw nlcIKh6sBtc")
+		fmt.Println("This will create synchronized versions of both tracks for mashups")
+		os.Exit(1)
+	}
+
+	id1 := os.Args[2]
+	id2 := os.Args[3]
+
+	// Initialize database to get BPM data
+	db, err := initDatabase()
+	if err != nil {
+		fmt.Printf("Error initializing database: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	// Get BPM data for both tracks
+	metadata1, found1 := getCachedMetadata(db, id1)
+	metadata2, found2 := getCachedMetadata(db, id2)
+
+	if !found1 || metadata1.BPM == nil {
+		fmt.Printf("Error: BPM data not found for %s. Run 'starchive bpm %s' first\n", id1, id1)
+		os.Exit(1)
+	}
+	if !found2 || metadata2.BPM == nil {
+		fmt.Printf("Error: BPM data not found for %s. Run 'starchive bpm %s' first\n", id2, id2)
+		os.Exit(1)
+	}
+
+	bpm1 := *metadata1.BPM
+	bpm2 := *metadata2.BPM
+
+	fmt.Printf("Synchronizing tracks:\n")
+	fmt.Printf("  %s: %.1f BPM\n", id1, bpm1)
+	fmt.Printf("  %s: %.1f BPM\n", id2, bpm2)
+
+	// Calculate tempo ratios
+	ratio1to2 := bpm2 / bpm1  // Speed up track 1 to match track 2
+	ratio2to1 := bpm1 / bpm2  // Speed up track 2 to match track 1
+
+	fmt.Printf("Tempo ratios:\n")
+	fmt.Printf("  %s->%s: %.3f (%.1f%%)\n", id1, id2, ratio1to2, (ratio1to2-1)*100)
+	fmt.Printf("  %s->%s: %.3f (%.1f%%)\n", id2, id1, ratio2to1, (ratio2to1-1)*100)
+
+	// Check if vocal separation has been done
+	if !metadata1.VocalDone {
+		fmt.Printf("Error: Vocal separation not done for %s. Run 'starchive vocal %s' first\n", id1, id1)
+		os.Exit(1)
+	}
+	if !metadata2.VocalDone {
+		fmt.Printf("Error: Vocal separation not done for %s. Run 'starchive vocal %s' first\n", id2, id2)
+		os.Exit(1)
+	}
+
+	// Process all files: vocals and instrumentals for both directions
+	files := []string{
+		fmt.Sprintf("%s_(Vocals)_UVR_MDXNET_Main.wav", id1),
+		fmt.Sprintf("%s_(Instrumental)_UVR_MDXNET_Main.wav", id1),
+		fmt.Sprintf("%s_(Vocals)_UVR_MDXNET_Main.wav", id2),
+		fmt.Sprintf("%s_(Instrumental)_UVR_MDXNET_Main.wav", id2),
+	}
+
+	ratios := []float64{ratio1to2, ratio1to2, ratio2to1, ratio2to1}
+	targetIds := []string{id2, id2, id1, id1}
+
+	for i, file := range files {
+		inputPath := fmt.Sprintf("./data/%s", file)
+		
+		// Check if input file exists
+		if _, err := os.Stat(inputPath); os.IsNotExist(err) {
+			fmt.Printf("Error: Input file %s does not exist\n", inputPath)
+			os.Exit(1)
+		}
+
+		// Create output filename
+		baseName := strings.TrimSuffix(file, ".wav")
+		outputPath := fmt.Sprintf("./data/%s_sync_to_%s.wav", baseName, targetIds[i])
+
+		fmt.Printf("\nProcessing: %s -> %s (ratio: %.3f)\n", file, filepath.Base(outputPath), ratios[i])
+
+		// Run rubberband
+		cmd := exec.Command("rubberband", 
+			"--time", fmt.Sprintf("%.6f", ratios[i]),
+			"--fine",  // Use R3 engine for better quality
+			"--formant", // Preserve formants for vocals
+			inputPath,
+			outputPath)
+
+		fmt.Printf("Running: %s\n", cmd.String())
+		
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		
+		err := cmd.Run()
+		if err != nil {
+			fmt.Printf("Error running rubberband: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Successfully created: %s\n", outputPath)
+	}
+
+	fmt.Printf("\nSync complete! Created synchronized versions of all tracks.\n")
+	fmt.Printf("Files created:\n")
+	for i, file := range files {
+		baseName := strings.TrimSuffix(file, ".wav")
+		outputName := fmt.Sprintf("%s_sync_to_%s.wav", baseName, targetIds[i])
+		fmt.Printf("  %s\n", outputName)
 	}
 }
 
